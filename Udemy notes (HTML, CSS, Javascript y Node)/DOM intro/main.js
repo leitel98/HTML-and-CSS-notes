771aa66911b38a2ebe0c3a1a4ed050288524f81a
leitel98 <leitel1@uc.cl>
2022-09-30 17:31:07 -0300

    Dom update

--- Udemy notes (HTML, CSS, Javascript y Node)/DOM intro/main.js
+++ Udemy notes (HTML, CSS, Javascript y Node)/DOM intro/main.js
@@ -1,1 +1,8 @@
-console.log('Dom intro from main')
+// window.onload nos asegura que primero cargara el html
+window.onload = () => {
+    const paragraph = document.getElementById('text')
+    // inner text cambia el texto de un ID de HTML
+    paragraph.innerText = 'updated by js'
+    // inner html cambia el tipo de contenedor que uso
+    paragraph.innerHTML = '<li>element 1</li><li>element 2</li>'
+}
